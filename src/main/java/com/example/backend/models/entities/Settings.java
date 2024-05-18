44ee6051709e09f82147bc3a0f67bdef7040cb49
enamilkovicc <milkoviccena@gmail.com>
2024-05-18 12:39:45 +0200

    spell check with change to camelcase

--- src/main/java/com/example/backend/models/entities/Settings.java
+++ src/main/java/com/example/backend/models/entities/Settings.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Settings {
     private long id;
-    private Integer death_check;
-    private Integer check_after_days;
+    private Integer deathCheck;
+    private Integer checkAfterDays;
     private Date deadline;
 }
--- src/main/java/com/example/backend/models/entities/Users.java
+++ src/main/java/com/example/backend/models/entities/Users.java
@@ -4,12 +4,12 @@ import java.rmi.server.UID;
 import java.util.Date;
 
 public class Users{
-    private long uid;
+    private long id;
     private String name;
     private String surname;
     private String email;
     private String password;
     private String status;
-    private Date last_loged_in;
+    private Date lastLoggedIn;
 }
 
