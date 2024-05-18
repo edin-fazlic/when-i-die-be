68f43dc0a2c02fd71f3407c746c3c5c41bb7472e
enamilkovicc <milkoviccena@gmail.com>
2024-05-18 15:02:18 +0200

    name changes of files

--- src/main/java/com/example/backend/controller/MessagesController.java
+++ src/main/java/com/example/backend/controller/MessagesController.java
@@ -2,6 +2,7 @@ package com.example.backend.controller;
 
 import com.example.backend.models.dtos.MessageListItemResponse;
 import com.example.backend.models.dtos.MessageRequest;
+import com.example.backend.services.MessageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,33 +12,35 @@ import java.util.List;
 @RequestMapping("/message")
 public class MessageController {
 
-    public MessageController() {}
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     @GetMapping("/all")
     public ResponseEntity<List<MessageListItemResponse>> getAllMessages() {
-
-        return null;
+        return messageService.getAllMessages();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<MessageRequest> getMessageById(@PathVariable Long id) {
-
-        return null;
+        return messageService.getMessageById(id);
     }
 
     @PostMapping("/{id}")
     public ResponseEntity<MessageRequest> updateMessage(@PathVariable Long id, @RequestBody MessageRequest request) {
-        return null;
+        return messageService.updateMessage(id);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<MessageRequest> deleteMessage(@PathVariable Long id) {
-        return null;
+        return messageService.deleteMessage(id);
     }
 
     @PutMapping("/create")
     public ResponseEntity<MessageRequest> createMessage(@RequestBody MessageRequest request) {
-        return null;
+        return messageService.createMessage();
     }
 
 
